292655377c1408e8b9ed3ab688c1b1591ad47b47
Rajendra Jagtap <rajendra.jagtap8595@gmail.com>
2024-05-01 00:02:53 +0530

    wp code

--- ec2.tf
+++ ec2.tf
@@ -79,7 +79,7 @@ resource "aws_lb" "wordpress_alb" {
   name               = "wordpress-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.wp_sg.id]
   subnets            = var.subnets
 
   tags = {
--- locals.tf
+++ locals.tf
@@ -4,4 +4,10 @@ locals {
     stage = "t2.micro"
     prod  = "t2.micro"
   }
+
+  subnet_ids = {
+    dev   = var.subnets[0]
+    stage = var.subnets[1]
+    prod  = var.subnets[2]
+  }
 }
