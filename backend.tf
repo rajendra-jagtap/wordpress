4051566aa8a07b355054a16919629fa81c92dd29
Rajendra Jagtap <rajendra.jagtap8595@gmail.com>
2024-04-30 23:53:58 +0530

    wp code

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "rajendra-terraform"
-    key    = "${terraform.workspace}-wordpress.tfstate"
+    #key    = "${terraform.workspace}-wordpress.tfstate"
     region = "ap-south-1"
   }
 }
